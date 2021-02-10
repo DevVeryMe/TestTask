9e2c1e51e340c62826dd76b53ef8530fc0b7f076
Andriy Veremiy <andrewveremiyy@gmail.com>
2021-02-10 15:52:56 +0200

    Implemented filtering and sorting.

--- TestTask/wwwroot/js/users.js
+++ TestTask/wwwroot/js/users.js
@@ -61,7 +61,7 @@ $('#users-table input').change(function() {
         'id': Number(table.rows[rowIndex].cells[0].innerText),
         'name': table.rows[rowIndex].cells[1].firstChild.value,
         'birthDate': table.rows[rowIndex].cells[2].firstChild.value,
-        'isMarried': (table.rows[rowIndex].cells[3].firstChild.value == 'True'),
+        'isMarried': (table.rows[rowIndex].cells[3].firstChild.value.toUpperCase == 'TRUE'),
         'phoneNumber': table.rows[rowIndex].cells[4].firstChild.value,
         'salary': Number(table.rows[rowIndex].cells[5].firstChild.value.replace(/,/g, '.'))
     }
@@ -80,3 +80,105 @@ $('#users-table input').change(function() {
         console.log("success");
     });
 });
+
+$("#search-input").keyup(function () {
+    // Declare variables
+    let td, i, txtValue;
+    let input = document.getElementById("search-input");
+    let filter = input.value.toUpperCase();
+    let table = document.getElementById("users-table");
+    let tr = table.getElementsByTagName("tr");
+
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+        let filtered = false;
+        let tds = tr[i].getElementsByTagName("td");
+        for (let t = 0; t < tds.length-1; t++) {
+            let td = tds[t];
+            if (td) {
+                if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                    filtered = true;
+                }
+            }
+        }
+        if (filtered === true) {
+            tr[i].style.display = '';
+        }
+        else {
+            tr[i].style.display = 'none';
+        }
+    }
+});
+
+function sortTable(n) {
+    var rows, switching, i, x, y, shouldSwitch, dir, switchCount = 0;
+    var table = document.getElementById("users-table");
+    switching = true;
+
+    dir = "asc";
+
+    while (switching) {
+
+        switching = false;
+        rows = table.rows;
+
+        for (i = 1; i < (rows.length - 1); i++) {
+
+            shouldSwitch = false;
+
+            x = rows[i].getElementsByTagName("TD")[n];
+            y = rows[i + 1].getElementsByTagName("TD")[n];
+
+            if (dir == "asc") {
+
+                let firstValue;
+                let secondValue;
+
+                if (n != 5) {
+                    firstValue = x.firstChild.value.toLowerCase();
+                    secondValue = y.firstChild.value.toLowerCase();
+                } else {
+                    firstValue = Number(x.firstChild.value.replace(/,/g, '.'));
+                    secondValue = Number(y.firstChild.value.replace(/,/g, '.'));
+                }
+
+                if (firstValue > secondValue) {
+
+                    shouldSwitch = true;
+                    break;
+                }
+            } else if (dir == "desc") {
+
+                let firstValue;
+                let secondValue;
+
+                if (n != 5) {
+                    firstValue = x.firstChild.value.toLowerCase();
+                    secondValue = y.firstChild.value.toLowerCase();
+                } else {
+                    firstValue = Number(x.firstChild.value.replace(/,/g, '.'));
+                    secondValue = Number(y.firstChild.value.replace(/,/g, '.'));
+                }
+
+                if (firstValue < secondValue) {
+
+                    shouldSwitch = true;
+                    break;
+                }
+            }
+        }
+        if (shouldSwitch) {
+
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+
+            switchCount++;
+        } else {
+
+            if (switchCount == 0 && dir == "asc") {
+                dir = "desc";
+                switching = true;
+            }
+        }
+    }
+}
